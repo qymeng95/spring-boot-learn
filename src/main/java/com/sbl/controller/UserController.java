16f55ffe4971fb566251fc1f5031bb285ada603c
qymeng <qymeng@i9i8.com>
2020-01-08 00:24:08 +0800

    # redis

--- src/main/java/com/sbl/controller/UserController.java
+++ src/main/java/com/sbl/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.sbl.controller;
 
+import com.sbl.config.RedisConfig;
 import com.sbl.model.User;
 import com.sbl.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,21 +14,33 @@ import java.util.List;
 
 @RestController
 public class UserController {
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @Autowired
     private UserRepository userRepository;
 
-    @RequestMapping("/getUser")
-    public User getUser() {
-        User user = userRepository.findByUserName("aa");
-        System.out.println("若下面没出现“无缓存的时候调用”字样且能打印出数据表示测试成功");
-        return user;
-    }
+//    @RequestMapping("/getUser")
+//    public User getUser() {
+//        User user = userRepository.findByUserName("aa");
+//        System.out.println("若下面没出现“无缓存的时候调用”字样且能打印出数据表示测试成功");
+//        return user;
+//    }
 
     @RequestMapping("/getUsers")
     public List<User> getUsers() {
         List<User> users = userRepository.findAll();
         System.out.println("若下面没出现“无缓存的时候调用”字样且能打印出数据表示测试成功");
         return users;
+    }
+
+    @RequestMapping("/getUser")
+    @Cacheable(value = "user-key")
+    public User getUser() {
+        redisTemplate.opsForValue().set("aaa", "111");
+        redisTemplate.opsForValue().get("aaa");
+        User user = new User("aa@126.com", "aa", "aa123456", "aa", "123");
+        System.out.println("若下面没出现“无缓存的时候调用”字样且能打印出数据表示测试成功");
+        return user;
     }
 }
